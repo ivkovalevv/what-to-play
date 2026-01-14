ac036632e9df544242e0405d1c768aa3f743eed3
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2026-01-14 07:40:31 +0000

    add header styles scroll-observer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,23 @@ import Logo from 'components/UI/Logo/Logo';
 import styles from './header.module.scss';
 import ProfileLink from 'components/UI/ProfileLink/ProfileLink';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
     const user = useAppSelector((state) => state.auth.user);
+    const [isHeaderScrolled, setIsHeaderScrolled] = useState<boolean>(false);
+
+    function checkVisibilityHeader(){
+        window.scrollY > 500 ? setIsHeaderScrolled(true) : setIsHeaderScrolled(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', checkVisibilityHeader)
+    }, []);
 
     return (
-        <header className={styles.header}>
-            <div className={styles.header__container}>
+        <header className={`${styles.header} ${isHeaderScrolled ? styles.header__scroll : ''}`}>
+            <div className={`${styles.header__container} ${isHeaderScrolled ? styles.header__container_scroll : ''}`}>
                 <Logo/>
                 {/* <Input className={styles['header-search']} size="large" placeholder="Limbo..." prefix={<SearchOutlined className={styles['header-search-icon']}/>} /> */}
                 {/* <SearchOutlined className={styles['header-search-icon']}/> */}

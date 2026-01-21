156c33489ca92e72eaa924cd5c6bf59c5f951663
ivkovalevv <127996062+ivkovalevv@users.noreply.github.com>
2026-01-21 16:34:18 +0000

    set the footer structure

--- src/UI/Signature/Signature.tsx
+++ src/UI/Signature/Signature.tsx
@@ -2,11 +2,16 @@ import styles from "./signature.module.scss"
 
 const Signatire = () => {
     return (
-        <p className={styles.signature}>
-            Created by 
-            <a href="https://github.com/ivkovalevv" target="_blank" className={styles.signature__link}>Ivkovalevv</a> 
-            © 2026
-        </p>
+        <div className={styles.signature__wrapper}>
+            <p className={styles.signature}>
+                Created by 
+                <a href="https://github.com/ivkovalevv" target="_blank" className={styles.signature__link}>Ivkovalevv</a> 
+                © 2026
+            </p>
+            <p className={styles.signature}>
+                All Rights Reserved
+            </p>
+        </div>
     );
 };
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,8 +9,43 @@ const Footer = () => {
                 <div className={`container ${styles.footer__container}`}>
                     <CircleLogo text={"What to play?"}/>
                     <div className={styles.footer__content_wrapper}>
-                        <div className={styles.footer__content}></div>
-                        <div className={styles.footer__content}></div>
+                        <div className={styles.footer__content_container}>
+                            <div className={styles.footer__content}>
+                                <p className={styles.footer__content_heading}>Sitemap</p>
+                                <ul className={styles.footer__content_list}>
+                                    <li className={styles.footer__content_list_item}>Home</li>
+                                    <li className={styles.footer__content_list_item}>Games List</li>
+                                    <li className={styles.footer__content_list_item}>Gaming senter</li>
+                                    <li className={styles.footer__content_list_item}>Contacts us</li>
+                                </ul>
+                            </div>
+                            <div className={styles.footer__content}>
+                                <p className={styles.footer__content_heading}>Company</p>
+                                <ul className={styles.footer__content_list}>
+                                    <li className={styles.footer__content_list_item}>Licensing</li>
+                                    <li className={styles.footer__content_list_item}>Cookie Policy</li>
+                                    <li className={styles.footer__content_list_item}>Privacy Policy</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles.footer__content_container}>
+                            <div className={styles.footer__content}>
+                                <p className={`${styles.footer__content_heading} ${styles.footer__content_heading_special}`}>Contacts</p>
+                                <ul className={`${styles.footer__content_list} ${styles.footer__content_list_special}`}>
+                                    <li className={styles.footer__content_list_item}>8 (915) 104-90-60</li>
+                                    <li className={styles.footer__content_list_item}>ivkovalevv@gmail.com</li>
+                                    <li className={styles.footer__content_list_item}>info@what-to-play.ru</li>
+                                </ul>
+                            </div>
+                            <div className={styles.footer__content}>
+                                <p className={`${styles.footer__content_heading} ${styles.footer__content_heading_special}`}>Socials</p>
+                                <ul className={`${styles.footer__content_list} ${styles.footer__content_list_special}`}>
+                                    <li className={styles.footer__content_list_item}>Github</li>
+                                    <li className={styles.footer__content_list_item}>Telegram</li>
+                                    <li className={styles.footer__content_list_item}>WhatsApp</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                     <Signatire/>
                 </div>
